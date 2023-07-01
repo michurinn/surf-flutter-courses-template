e9faa3713c8bbb2c52358804ab8344415fe27134
michurinnn <michurinnn@gmail.com>
2023-07-01 14:38:52 +0300

    init

--- README.md
+++ README.md
@@ -1,23 +1,42 @@
 # Description of your result
 
-You can describe your results in any language that is convenient for you, not just English.
+Задание — GestureMaster
+Предлагаем вам реализовать "слишком" интерактивный виджет!
+
+Например, это может быть обычный квадрат, но если для вас он слишком скучный, можете использовать любое фото или иконку.
+
+Растяните виджет `Stack` по всему экрану и оберните ваш виджет в `Positioned`. Он позволяет зафиксировать расположение виджета на экране. Пусть его начальная позиция будет центре экрана
+За ним в `Stack` поместите `GestureDetector` обернутый в `Positioned.fill`, чтобы он занял весь `Stack`.
+Найдите подходящие callback'и, которые можно передать в параметры `GestureDetector`, чтобы в них передавались данные, которые помогут вам реализовать некоторые трансформации.
+А именно:
+
+изменение положение вашего виджета на drag;
+изменение формы или цвета на tap;
+вращение виджета на long press.
+Добавлять фильтр жестов, чтобы они происходили непосредственно с вашим виджетом — не обязательно. Пусть жест в любом месте экрана может повлиять на положение/форму/цвет/вращение вашего виджета.
+
+Для реализации вам придется освежить знания из предыдущих тем про виджеты.
+
+Также вам могут помочь виджеты AnimatedContainer и RotationTransition.
+
 
 ## Description
 
-[Describe what you have done]
-
-## How to
-
-[Describe how to run your application if there are any issues]
-
+1. Stack находится в корне поддерева виджетов, возвращаемого методом build в _LogoWidgetState, поетому он автоматически занимает всё пространство родителя ( в нашем случае екрана).
+2. Поскольку мы используем Positioned для расположения Лого внутри стека, используем размер екрана и размер Лого для вычисления отступов.
+Если не учитывать размер Лого, то виджет отобразится так, что его левый нижний угол будет находиться в центре екрана.
+3. Трансформации реализованы:
+    3а. /изменение положение вашего виджета на drag/ callback onHorizontalDragUpdate с использованием details.delta.dx, 
+                                                     callback onVerticalDragUpdate   с использованием details.delta.dy,
+    3b. /вращение виджета на long press/             callback onLongPressEnd запускает анимацию в прямом/обратном порядке,
+                                                                                которую слушает виджет RotationTransition
+    3с. /изменение формы или цвета на tap/           callback onTap, который вызывает setState, обновляя конфигурацию виджета FlutterLogo
 ## Problems
 
-[Describe the problems you encountered]
-
+У меня не работает буква Э, поетому везде Е
 ## More
 
-[Here you can brag about what you managed to implement or do something beyond the task]
-
+FlutterLogo я выбрал до того, как узнал, что при етом выборе для решения задачи не потребуется использовать виджет AnimatedContainer 
 ## Demo
 
-[Leave links to screencasts/screenshots here, you can use a GitHub repository or a separate cloud service]
+https://disk.yandex.ru/i/DoO6rNYR5N1VRw
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +11,103 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: LogoWidget());
+  }
+}
+
+class LogoWidget extends StatefulWidget {
+  const LogoWidget({super.key});
+
+  @override
+  State<LogoWidget> createState() => _LogoWidgetState();
+}
+
+class _LogoWidgetState extends State<LogoWidget> with TickerProviderStateMixin {
+  late FlutterLogoStyle style;
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+  late double x0, y0;
+  final double size = 200.0;
+  @override
+  void initState() {
+    super.initState();
+
+    style = FlutterLogoStyle.horizontal;
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInCubic,
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    x0 = MediaQuery.of(context).size.width / 2;
+    y0 = MediaQuery.of(context).size.height / 2;
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          Positioned(
+            top: y0 -
+                size /
+                    2, // Чтобы в центре екрана находился центр логотипа, а не начальная точка
+            left: x0 -
+                size /
+                    2, // Чтобы в центре екрана находился центр логотипа, а не начальная точка
+            child: RotationTransition(
+              turns: _animation,
+              child: FlutterLogo(
+                style: style,
+                size: size,
+              ),
+            ),
+          ),
+          Positioned.fill(
+              child: GestureDetector(
+            onTap: () => setState(() {
+              late FlutterLogoStyle newstyle;
+              // При нажатии всегда меняется виджет
+              do {
+                newstyle = FlutterLogoStyle.values[Random().nextInt(3)];
+              } while (style == newstyle);
+              style = newstyle;
+            }),
+            onHorizontalDragUpdate: (details) {
+              setState(() {
+                x0 += details.delta.dx;
+                x0 = x0.clamp(
+                    size / 2, MediaQuery.of(context).size.width - size / 2);
+              });
+            },
+            onVerticalDragUpdate: (details) {
+              setState(() {
+                y0 += details.delta.dy;
+                y0 = y0.clamp(
+                    size / 2, MediaQuery.of(context).size.height - size / 2);
+              });
+            },
+            onLongPressEnd: (details) {
+              _controller.status == AnimationStatus.completed
+                  ? _controller.reverse()
+                  : _controller.forward();
+            },
+          ))
+        ],
       ),
     );
   }
