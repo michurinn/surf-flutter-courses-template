1c5b68fdf53e2d2420b0b0454da1395054dd4925
michurinnn <michurinnn@gmail.com>
2023-10-23 16:27:03 +0300

    getters for Red,Green,Blue components of colrs have added in the ColorClass

--- lib/domain/color.dart
+++ lib/domain/color.dart
@@ -1,5 +1,7 @@
 // Класс для храниения параметров цветов для палитры
 // Название ColorClass, чтобы отличался от Dart-класса Color
+import 'dart:ui';
+
 class ColorClass {
   //Название цвета
   final String name;
@@ -7,4 +9,18 @@ class ColorClass {
   final String value;
 
   ColorClass({required this.name, required this.value});
+
+  // Получение цвета, соответсвующего lue
+  Color get hexColor => Color(int.parse('0xff${value.substring(1)}'));
+
+  // Получить hex значение цвета без #
+  String get clearValue => value.substring(1);
+
+  // Получить составляющую Red
+  String get redPart => clearValue.substring(0, 2);
+  // Получить составляющую Green
+  String get greenPart => clearValue.substring(2, 4);
+
+  // Получить составляющую Blue
+  String get bluePart => clearValue.substring(4);
 }
