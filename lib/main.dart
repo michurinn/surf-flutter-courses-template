95b05955cf08cc7abdd388b4e2fc286838c3b8b6
michurinnn <michurinnn@gmail.com>
2023-12-26 09:36:03 +0300

    Home Screen? interactor for user photos and default theme have been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:surf_flutter_courses_template/data/repository/profile_pictures_repository/profile_picture_repository_current.dart';
+import 'package:surf_flutter_courses_template/interactors/profile_picture_interactor.dart';
+import 'package:surf_flutter_courses_template/screen/profile_pictures_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +13,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
+      ),
+      home: Provider(
+        create: (context) => ProfilePicturesInteractor(
+          ProfilePictureRepository(),
+        ),
+        child: const ProfilePicturesScreen(),
       ),
     );
   }
