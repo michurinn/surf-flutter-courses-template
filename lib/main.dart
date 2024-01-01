11eb8314e9d8d3239c7630835874ad0535f0dfb0
michurinnn <michurinnn@gmail.com>
2024-01-01 16:24:31 +0300

    Main.dart has been described

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:surf_flutter_courses_template/bloc/magic_ball_bloc/magic_ball_cubit.dart';
+import 'package:surf_flutter_courses_template/data/http_client/dio_http_client.dart';
+import 'package:surf_flutter_courses_template/data/repository/magic_ball_answers_repository/magic_ball_answers_repository.dart';
+import 'package:surf_flutter_courses_template/data/rest_client/rest_client.dart';
+import 'package:surf_flutter_courses_template/presentation/magic_ball_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +16,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ScreenUtilInit(
+      designSize: const Size(393, 857),
+      child: MaterialApp(
+        home: BlocProvider(
+          create:(context) => MagicBallCubit(
+            magicBallAnswersRepository: MagicBallAnswersRepository(
+              restClient: RestClient(DioHttpClient().dio),
+            ),
+          ),
+          child: const MaigicBallScreen(),
         ),
       ),
     );
